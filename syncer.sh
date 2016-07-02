6d0ca149d6cd324fc224e6b1644b0c2551c4687a
Simon Tucker <brotherlogic@gmail.com>
2016-07-02 10:47:53 -0700

    Quick fix to assembly configuration

--- syncer.sh
+++ syncer.sh
@@ -1,3 +1,3 @@
 rm card.jar
 rsync -avz --progress 10.0.1.17:codestore ./
-ln -s $(find codestore/ | grep .jar$ | sort -n | tail -n 1) ./card.jar
+ln -s $(find codestore/ | grep jar-with-dependencies.jar$ | sort -n | tail -n 1) ./card.jar
