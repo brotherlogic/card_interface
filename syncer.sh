68de8d36a02e0a3174645116b6a9a0dc515ebbc7
Simon Tucker <brotherlogic@gmail.com>
2016-07-03 11:22:45 -0700

    Quick fix to syncer script

--- syncer.sh
+++ syncer.sh
@@ -1,3 +1,3 @@
 rm card.jar
-rsync -avz --progress 10.0.1.17:codestore ./
+rsync -avz --progress 10.0.1.17:codestore/card_ui/ ./
 ln -s $(find codestore/ | grep jar-with-dependencies.jar$ | sort -n | tail -n 1) ./card.jar
