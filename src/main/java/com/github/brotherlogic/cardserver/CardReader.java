bedbcea8275b8c1b9f1695e8a3df2dbbb173d703
Simon Tucker <brotherlogic@gmail.com>
2016-07-26 19:56:36 -0700

    Fix updates to every minute

--- src/main/java/com/github/brotherlogic/cardserver/CardReader.java
+++ src/main/java/com/github/brotherlogic/cardserver/CardReader.java
@@ -6,21 +6,21 @@ import card.CardOuterClass.Card;
 
 public abstract class CardReader {
 
-	//Update every 60 seconds
-	private final int WAIT_TIME = 1000*10;
-	
+	// Update every 60 seconds
+	private final int WAIT_TIME = 1000 * 60;
+
 	public abstract List<Card> readCards();
-	
-	public void readCardsBackground(CardsReturned callback){
-		while(true){			
+
+	public void readCardsBackground(CardsReturned callback) {
+		while (true) {
 			if (CardInterface.refresh)
 				callback.processCards(readCards());
-			try{
+			try {
 				Thread.sleep(WAIT_TIME);
-			} catch (InterruptedException e){
+			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
 }
