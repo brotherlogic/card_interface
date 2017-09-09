d08d25bab9accb7b92ab06cdfaf1c9c70e93c1be
Simon Tucker <brotherlogic@gmail.com>
2017-09-08 17:07:07 -0700

    Fixed syncer

--- syncer.sh
+++ syncer.sh
@@ -1,3 +1,6 @@
+git fetch -p
+git merge origin/master
+git gc
 rm card.jar
 #rsync -avz --progress --delete-after 192.168.86.64:codestore/card_ui/ ./codestore
 ln -s $(find codestore/ | grep jar-with-dependencies.jar$ | sort -n | tail -n 1) ./card.jar
