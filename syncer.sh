0ab0f4211ce21c9ae4d7634b2be2d6d144cc707d
Simon Tucker <brotherlogic@gmail.com>
2017-09-27 08:57:13 -0700

    Trying this

--- syncer.sh
+++ syncer.sh
@@ -2,5 +2,5 @@ git fetch -p
 git merge origin/master
 git gc
 rm card.jar
-rsync -avz --progress --delete-after 192.168.86.64:codestore/card_ui/ ./codestore
+rsync -avz --progress --delete-after 192.168.86.26:codestore/card_ui/ ./codestore
 ln -s $(find codestore/ | grep jar-with-dependencies.jar$ | sort -n | tail -n 1) ./card.jar
